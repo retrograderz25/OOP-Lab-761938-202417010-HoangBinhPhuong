17ea46a1b3a63e773b741a08702c8fa1b6215e4c
Hoàng Bình Phương <hereda.unknown@gmail.com>
2026-04-05 16:26:27 +0700

    feat: aims project & DigitalVideoDisc class initialized

---
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -0,0 +1,10 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    private int id;
+
+    private static int nbDigitalVideoDiscs = 0; // Biến đếm số lượng DVD
+}
