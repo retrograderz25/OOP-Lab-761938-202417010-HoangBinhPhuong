8f3794ce3b20ad3d02b57415fe7b6a479a3f03ee
Hoàng Bình Phương <hereda.unknown@gmail.com>
2026-04-05 16:41:00 +0700

    feat: add getters in DVD class

--- Lab02/AimsProject/src/DigitalVideoDisc.java
+++ Lab02/AimsProject/src/DigitalVideoDisc.java
@@ -7,4 +7,28 @@ public class DigitalVideoDisc {
     private int id;
 
     private static int nbDigitalVideoDiscs = 0; // Biến đếm số lượng DVD
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
